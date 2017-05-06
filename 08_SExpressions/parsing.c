55954f0e308ac092c057781b3faece438638a229
Kyle Oba <koba@mudphone.com>
2017-05-06 00:06:38 -0500

    Add min, max, and power

--- 08_SExpressions/parsing.c
+++ 08_SExpressions/parsing.c
@@ -198,6 +198,9 @@ lval* builtin_op(lval* a, char* op) {
     if (strcmp(op, "+") == 0) { x->num += y->num; }
     if (strcmp(op, "-") == 0) { x->num -= y->num; }
     if (strcmp(op, "*") == 0) { x->num *= y->num; }
+    if (strcmp(op, "^") == 0) { x->num = pow(x->num, y->num); }
+    if (strcmp(op, "min") == 0) { x->num = x->num > y->num ? y->num : x->num; }
+    if (strcmp(op, "max") == 0) { x->num = x->num > y->num ? x->num : y->num; }
     if (strcmp(op, "/") == 0) {
       if (y->num == 0) {
         lval_del(x); lval_del(y);
